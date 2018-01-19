edaea57749d0687212577b9782ef97df88413335
German Laullon <glaullon@vmware.com>
2018-01-19 15:39:02 +0100

    review comments

--- src/main/java/com/wavefront/websphere/InfoServlet.java
+++ src/main/java/com/wavefront/websphere/InfoServlet.java
@@ -7,10 +7,10 @@ package com.wavefront.websphere;
 
 import com.ibm.json.java.JSONObject;
 import com.ibm.websphere.management.AdminServiceFactory;
-import static com.wavefront.websphere.DafaultServlet.getStackTrace;
+
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Set;
+
 import javax.management.MBeanServer;
 import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
@@ -19,85 +19,83 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import static com.wavefront.websphere.DafaultServlet.getPrefObjectName;
+import static com.wavefront.websphere.DafaultServlet.getStackTrace;
+
 public class InfoServlet extends HttpServlet {
 
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String wasVersion = "";
-        JSONObject info = new JSONObject();
+  /**
+   * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+   *
+   * @param request  servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException      if an I/O error occurs
+   */
+  protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    String wasVersion = "";
+    JSONObject info = new JSONObject();
 
-        MBeanServer server = AdminServiceFactory.getMBeanFactory().getMBeanServer();
-        try {
-            ObjectName queryName = new ObjectName("WebSphere:type=Perf,*");
-            Set<ObjectName> perfNames = server.queryNames(queryName, null);
-            for (ObjectName perfName : perfNames) {
-                wasVersion = perfName.getKeyProperty("version");
-            }
-        } catch (MalformedObjectNameException ex) {
-            info.put("error", ex.getMessage());
-            info.put("stack", getStackTrace(ex));
-        }
-
-        java.util.Properties prop = new java.util.Properties();
-        prop.load(getServletContext().getResourceAsStream("/META-INF/MANIFEST.MF"));
-        String warVersion = prop.getProperty("version");
-
-        info.put("wasmonitor.version", warVersion);
-        info.put("websphere.version", wasVersion);
-
-        try (PrintWriter out = response.getWriter()) {
-            info.serialize(out);
-        }
-
+    MBeanServer server = AdminServiceFactory.getMBeanFactory().getMBeanServer();
+    try {
+      ObjectName perfName = getPrefObjectName(server);
+      wasVersion = perfName.getKeyProperty("version");
+    } catch (MalformedObjectNameException ex) {
+      info.put("error", ex.getMessage());
+      info.put("stack", getStackTrace(ex));
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    java.util.Properties prop = new java.util.Properties();
+    prop.load(getServletContext().getResourceAsStream("/META-INF/MANIFEST.MF"));
+    String warVersion = prop.getProperty("version");
+
+    info.put("wasmonitor.version", warVersion);
+    info.put("websphere.version", wasVersion);
+
+    try (PrintWriter out = response.getWriter()) {
+      info.serialize(out);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+  }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+  /**
+   * Handles the HTTP <code>GET</code> method.
+   *
+   * @param request  servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException      if an I/O error occurs
+   */
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    processRequest(request, response);
+  }
+
+  /**
+   * Handles the HTTP <code>POST</code> method.
+   *
+   * @param request  servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException      if an I/O error occurs
+   */
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    processRequest(request, response);
+  }
+
+  /**
+   * Returns a short description of the servlet.
+   *
+   * @return a String containing servlet description
+   */
+  @Override
+  public String getServletInfo() {
+    return "Short description";
+  }
 
 }
--- src/main/java/com/wavefront/websphere/ListServlet.java
+++ src/main/java/com/wavefront/websphere/ListServlet.java
@@ -8,12 +8,14 @@ package com.wavefront.websphere;
 import com.ibm.json.java.JSONArray;
 import com.ibm.json.java.JSONObject;
 import com.ibm.websphere.management.AdminServiceFactory;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.management.ManagementFactory;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+
 import javax.management.InstanceNotFoundException;
 import javax.management.IntrospectionException;
 import javax.management.MBeanServer;
@@ -27,63 +29,63 @@ import javax.servlet.http.HttpServletResponse;
 
 public class ListServlet extends DafaultServlet {
 
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    @Override
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        JSONArray res = new JSONArray();
+  private static JSONArray dumpObject(MBeanServerConnection server, Set<ObjectName> objectsNames) throws IOException, ReflectionException, IntrospectionException {
+    JSONArray res = new JSONArray();
+    for (ObjectName objectName : objectsNames) {
+      JSONObject object = new JSONObject();
+      object.putAll(getTags(objectName));
+      try {
+        object.put("Attributes", Arrays.toString(server.getMBeanInfo(objectName).getAttributes()));
+      } catch (InstanceNotFoundException ex) {
+        object.put("Attributes", ex.toString());
+      }
+      try {
+        object.put("Operations", Arrays.toString(server.getMBeanInfo(objectName).getOperations()));
+      } catch (InstanceNotFoundException ex) {
+        object.put("Operations", ex.toString());
+      }
+      res.add(object);
+    }
+    return res;
+  }
 
-        Set<MBeanServer> servers = new HashSet<>();
-        servers.add(ManagementFactory.getPlatformMBeanServer());
-        servers.add(AdminServiceFactory.getMBeanFactory().getMBeanServer());
+  @Override
+  protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    JSONArray res = new JSONArray();
 
-        for (MBeanServer server : servers) {
-            try {
-                ObjectName queryName = new ObjectName("*:*");
-                Set<ObjectName> objectsNames = new HashSet<>();
-                objectsNames.addAll(server.queryNames(queryName, null));
-                res.addAll(dumpObject(server, objectsNames));
-            } catch (MalformedObjectNameException | ReflectionException | IntrospectionException ex) {
-                JSONObject error = new JSONObject();
-                error.put("error", ex.getMessage());
-                error.put("stack", getStackTrace(ex));
-                res.add(error);
-            }
-        }
+    Set<MBeanServer> servers = new HashSet<>();
+    servers.add(ManagementFactory.getPlatformMBeanServer());
+    servers.add(AdminServiceFactory.getMBeanFactory().getMBeanServer());
 
-        try (PrintWriter out = response.getWriter()) {
-            res.serialize(out);
-        }
+    for (MBeanServer server : servers) {
+      try {
+        ObjectName queryName = new ObjectName("*:*");
+        Set<ObjectName> objectsNames = new HashSet<>();
+        objectsNames.addAll(server.queryNames(queryName, null));
+        res.addAll(dumpObject(server, objectsNames));
+      } catch (MalformedObjectNameException | ReflectionException | IntrospectionException ex) {
+        JSONObject error = new JSONObject();
+        error.put("error", ex.getMessage());
+        error.put("stack", getStackTrace(ex));
+        res.add(error);
+      }
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "List all mBeans";
+    try (PrintWriter out = response.getWriter()) {
+      res.serialize(out);
     }
+  }
 
-    private static JSONArray dumpObject(MBeanServerConnection server, Set<ObjectName> objectsNames) throws IOException, ReflectionException, IntrospectionException {
-        JSONArray res = new JSONArray();
-        for (ObjectName objectName : objectsNames) {
-            JSONObject object = new JSONObject();
-            object.putAll(getTags(objectName));
-            try {
-                object.put("Attributes", Arrays.toString(server.getMBeanInfo(objectName).getAttributes()));
-            } catch (InstanceNotFoundException ex) {
-                object.put("Attributes", ex.toString());
-            }
-            try {
-                object.put("Operations", Arrays.toString(server.getMBeanInfo(objectName).getOperations()));
-            } catch (InstanceNotFoundException ex) {
-                object.put("Operations", ex.toString());
-            }
-            res.add(object);
-        }
-        return res;
-    }
+  /**
+   * Returns a short description of the servlet.
+   *
+   * @return a String containing servlet description
+   */
+  @Override
+  public String getServletInfo() {
+    return "List all mBeans";
+  }
 
 }
